d6a979c6cc8ba54b7b7e8b46d5615a0836402842
Shahid <shahidnazirmhd@gmail.com>
2023-12-28 17:04:48 +0530

    setting DB

---
+++ customer/src/main/java/com/example/customer/CustomerService.java
@@ -0,0 +1,15 @@
+package com.example.customer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public record CustomerService() {
+    public void registerCustomer(CustomerRegistrationRequest request) {
+        Customer customer = Customer.builder()
+                .firstName(request.firstName())
+                .lastName(request.lastName())
+                .email(request.email())
+                .build();
+        // TODO: 28-12-2023 validation
+    }
+}
