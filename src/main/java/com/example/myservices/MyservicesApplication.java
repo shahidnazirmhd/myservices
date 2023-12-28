68de41d0a27c7f7693bca4fb5a0e83bf74c4cf96
Shahid <shahidnazirmhd@gmail.com>
2023-12-28 12:18:22 +0530

    Initial commit

---
+++ src/main/java/com/example/myservices/MyservicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.myservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myservices/MyservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
