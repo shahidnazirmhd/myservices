971d339a0424552c38e86de343a2bd87b1a7dad7
shahidnazirmhd <shahidnazirmhd@gmail.com>
2023-12-31 21:03:08 +0530

    docker configured

--- customer/src/main/java/com/example/customer/Customer.java
+++ customer/src/main/java/com/example/customer/Customer.java
@@ -6,7 +6,7 @@ import lombok.Data;
 @Data
 @Builder
 public class Customer {
-    private Integer id;
+    private Long id;
     private String firstName;
     private String lastName;
     private String email;
